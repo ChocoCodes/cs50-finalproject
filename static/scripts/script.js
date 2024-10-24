f450d9ea3476d65020b94da0579ffb077d383e99
ChocoCodes <chocojhong999@gmail.com>
2024-10-24 09:38:02 +0800

    Revised password visibility checkbox

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,9 +1,21 @@
 let btnPressedId;
 let hasAddedComponents = false;
+let isPasswordVisible = false;
 
-const changeVisibility = (checkbox) => {
+const changeVisibility = () => {
     const passField =  document.getElementById("pass");
-    checkbox.checked ? passField.type = "text" : passField.type = "password";
+    const toggle = document.getElementById("password-toggle")
+    isPasswordVisible = !isPasswordVisible;
+    passField.type = isPasswordVisible ? "text" : "password";
+    toggle.innerHTML = isPasswordVisible ? 
+    `<path d="M2 8C2 8 6.47715 3 12 3C17.5228 3 22 8 22 8" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+    <path d="M21.544 13.045C21.848 13.4713 22 13.6845 22 14C22 14.3155 21.848 14.5287 21.544 14.955C20.1779 16.8706 16.6892 21 12 21C7.31078 21 3.8221 16.8706 2.45604 14.955C2.15201 14.5287 2 14.3155 2 14C2 13.6845 2.15201 13.4713 2.45604 13.045C3.8221 11.1294 7.31078 7 12 7C16.6892 7 20.1779 11.1294 21.544 13.045Z" stroke="currentColor" stroke-width="1.5" />
+    <path d="M15 14C15 12.3431 13.6569 11 12 11C10.3431 11 9 12.3431 9 14C9 15.6569 10.3431 17 12 17C13.6569 17 15 15.6569 15 14Z" stroke="currentColor" stroke-width="1.5" />
+    ` : 
+    `<path d="M19.439 15.439C20.3636 14.5212 21.0775 13.6091 21.544 12.955C21.848 12.5287 22 12.3155 22 12C22 11.6845 21.848 11.4713 21.544 11.045C20.1779 9.12944 16.6892 5 12 5C11.0922 5 10.2294 5.15476 9.41827 5.41827M6.74742 6.74742C4.73118 8.1072 3.24215 9.94266 2.45604 11.045C2.15201 11.4713 2 11.6845 2 12C2 12.3155 2.15201 12.5287 2.45604 12.955C3.8221 14.8706 7.31078 19 12 19C13.9908 19 15.7651 18.2557 17.2526 17.2526" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M9.85786 10C9.32783 10.53 9 11.2623 9 12.0711C9 13.6887 10.3113 15 11.9289 15C12.7377 15 13.47 14.6722 14 14.1421" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+    <path d="M3 3L21 21" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    `;
 }
 
 const formatToPHP = (amount) => `₱${amount.toFixed(2)}`;
